3a777e470c57e870059dc070a4641d6c0eb176f0
AmjadNoor <nooramjadmail.com@gmail.com>
2021-02-18 12:19:52 +0530

    Javascript added

--- circle/main.js
+++ circle/main.js
@@ -5,3 +5,13 @@ function openNav(){
 function closeNav(){
   document.getElementById('sidenav').style.width = "0";
 }
+function check() {
+  const fig = 'Circle';
+  const pi = 22/7;
+  let r = document.getElementById('inp1').value;
+  const circleArea = pi * r ** 2;
+
+  if (fig) {
+    document.getElementById('para1').innerHTML = `The area of circle is ${circleArea}`;
+  }
+}
