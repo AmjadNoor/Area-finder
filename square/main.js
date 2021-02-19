5595e5454ff661d9fa82b34614adbd3c0d2f3793
AmjadNoor <nooramjadmail.com@gmail.com>
2021-02-19 18:05:36 +0530

    Square folder completed

--- square/main.js
+++ square/main.js
@@ -5,3 +5,12 @@ function openNav(){
 function closeNav(){
   document.getElementById('sidenav').style.width = "0";
 }
+function check(){
+  const fig = "Square";
+  let s = document.getElementById('inp1').value;
+  const squareArea = s ** 2;
+  
+  if (fig) {
+    document.getElementById('para1').innerHTML = `The area of square is ${squareArea}.`
+  }
+}
