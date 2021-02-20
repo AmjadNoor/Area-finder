7a189cbf5b40976bae8d1d2954c25c3b7e583210
AmjadNoor <nooramjadmail.com@gmail.com>
2021-02-20 11:46:30 +0530

    Triangle folder completed

--- triangle/main.js
+++ triangle/main.js
@@ -5,3 +5,12 @@ function openNav(){
 function closeNav(){
   document.getElementById('sidenav').style.width = "0";
 }
+function check(){
+  const fig = "Triangle"
+  let b = document.getElementById('inp1').value;
+  let h = document.getElementById('inp2').value;
+  const triangleArea = (b * h) / 2;
+  if(fig){
+    document.getElementById('para1').innerHTML = `The area of triangle is ${triangleArea}.`
+  }
+}
